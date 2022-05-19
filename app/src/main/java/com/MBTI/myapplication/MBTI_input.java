f4743807682c3a26d6774fc29cb8da558d08a9d6
Hippoya <hanah9082@naver.com>
2022-05-19 15:08:21 +0900

    버튼 작업, 폰트 추가, mbti 종류 16가지 설정

--- app/src/main/java/com/MBTI/myapplication/MBTI_input.java
+++ app/src/main/java/com/MBTI/myapplication/MBTI_input.java
@@ -6,24 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.content.SharedPreferences;
 
 public class MBTI_input extends AppCompatActivity {
-
-    EditText et_mbti;
-    Button btn_ok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mbti_input);
 
-        et_mbti = findViewById(R.id.et_mbti);
-        btn_ok = findViewById(R.id.btn_ok);
-        btn_ok.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                et_mbti.setText("estp");
-            }
-        });
     }
 }
