86593bcf2253d1aaa20d12a56ca0458ec2256f9a
Hippoya <hanah9082@naver.com>
2022-05-19 11:14:48 +0900

    first commit

---
+++ app/src/main/java/com/MBTI/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.MBTI.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler x = new Handler();
+        x.postDelayed(new SplashHandler(), 2000);
+    }
+
+    public class SplashHandler implements Runnable {
+        public void run(){
+            Intent intent = new Intent(getApplicationContext(),
+                    MBTI_input.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
