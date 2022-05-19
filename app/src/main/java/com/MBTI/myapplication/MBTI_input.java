0fc340b4fe2ea0afec3a9f127cce7d754b800d81
Hippoya <hanah9082@naver.com>
2022-05-19 15:28:36 +0900

    잘모르겠어요 버튼 누를시 테스트 액티비티 전환

--- app/src/main/java/com/MBTI/myapplication/MBTI_input.java
+++ app/src/main/java/com/MBTI/myapplication/MBTI_input.java
@@ -2,6 +2,7 @@ package com.MBTI.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,5 +16,14 @@ public class MBTI_input extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mbti_input);
 
+        //잘모르겠어요 버튼 클릭시 테스트 화면 이동
+        Button btn_unknown = (Button)findViewById(R.id.btn_unknown);
+        btn_unknown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),unknown.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/MBTI/myapplication/MBTI_test.java
@@ -0,0 +1,14 @@
+package com.MBTI.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MBTI_test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mbti_test);
+    }
+}
