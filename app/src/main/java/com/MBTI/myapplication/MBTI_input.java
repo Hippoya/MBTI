67a71938b6a467f77ba9a3ecb3044ca931027e24
HyunSoo98 <irver456@naver.com>
2022-05-25 23:28:52 +0900

    MBTI_input의 intj 버튼 누르면 홈 화면으로 가도록 추가

--- app/src/main/java/com/MBTI/myapplication/MBTI_input.java
+++ app/src/main/java/com/MBTI/myapplication/MBTI_input.java
@@ -15,14 +15,30 @@ public class MBTI_input extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mbti_input);
 
+        //INTJ 버튼 클릭시 테스트 화면 이동
+        Button btn_intj = (Button)findViewById(R.id.btn_intj);
+        btn_intj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), home.class);
+                    startActivity(intent);
+            }
+        });
+
+
+
+
+
         //잘모르겠어요 버튼 클릭시 테스트 화면 이동
         Button btn_unknown = (Button)findViewById(R.id.btn_unknown);
         btn_unknown.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),unknown.class);
-                startActivity(intent);
+                    Intent intent = new Intent(getApplicationContext(), unknown.class);
+                    startActivity(intent);
             }
         });
+
+
     }
 }
