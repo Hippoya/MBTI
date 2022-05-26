80a8cfeddd2914b26e7f681d9a5e16290b03679c
0yewon0 <anyangel4471@naver.com>
2022-05-26 14:48:47 +0900

    각 intro 페이지에서 버튼 누르면 해당 mbti 여행지 리스트로 넘어가게 구현 또한 여행지 리스트에서 여행지 이름 클릭하면 그 페이지로 가게 구현

--- app/src/main/java/com/MBTI/myapplication/intj_intro.java
+++ app/src/main/java/com/MBTI/myapplication/intj_intro.java
@@ -2,7 +2,10 @@ package com.MBTI.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class intj_intro extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class intj_intro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intj_intro);
+
+        Button btn_intj_list = (Button)findViewById(R.id.btn_intj_list);
+        btn_intj_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), intj_list.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/MBTI/myapplication/intj_list.java
@@ -0,0 +1,51 @@
+package com.MBTI.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class intj_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intj_list);
+
+        ListView listView = findViewById(R.id.intj_list);
+
+        List<String> list = new ArrayList<>();
+        list.add("석굴암");
+        list.add("불국사");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+//                String data = (String) adapterView.getItemAtPosition(position);
+
+                Intent intent;
+
+                switch(position)
+                {
+                    case 0:
+                        intent = new Intent(getApplicationContext(), intj1.class);
+                        startActivity(intent);
+                        break;
+                }
+
+
+
+            }
+        });
+    }
+}
