12d83f024f76ac14cc80ab6fe6acc16fe2d091dd
0yewon0 <anyangel4471@naver.com>
2022-05-26 16:37:37 +0900

    enfj, enfp intro 페이지에서 버튼 클릭하면 어플 종료됨

---
+++ app/src/main/java/com/MBTI/myapplication/list/enfj/enfj_list.java
@@ -0,0 +1,22 @@
+package com.MBTI.myapplication.list.enfj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.List;
+import java.util.ArrayList;
+import com.MBTI.myapplication.R;
+
+public class enfj_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enfj_list);
+    }
+}
---
+++ app/src/main/java/com/MBTI/myapplication/list/istj/istj_list.java
@@ -0,0 +1,21 @@
+package com.MBTI.myapplication.list.istj;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.List;
+import java.util.ArrayList;
+import com.MBTI.myapplication.R;
+
+public class istj_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_istj_list);
+    }
+}
