47ab1379b7bbc222b0ea753ea484a6cffd0e3cdf
Hippoya <hanah9082@naver.com>
2022-05-26 10:27:42 +0900

    16가지 액티비티 설명란 추가

--- app/src/main/java/com/MBTI/myapplication/MBTI_test.java
+++ app/src/main/java/com/MBTI/myapplication/MBTI_test.java
@@ -2,7 +2,9 @@ package com.MBTI.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MBTI_test extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MBTI_test extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mbti_test);
+
+        Handler x = new Handler();
+        x.postDelayed(new MBTI_test.SplashHandler(), 2000);
+    }
+
+    public class SplashHandler implements Runnable {
+        public void run(){
+            Intent intent = new Intent(getApplicationContext(),
+                    what_mbti.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
