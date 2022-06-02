a3ed6d1d9ea13dcdce9e88b40e102cf57946afda
HyunSoo98 <irver456@naver.com>
2022-06-02 15:08:13 +0900

    .

--- app/src/main/java/com/MBTI/myapplication/list/intj/intj1.java
+++ app/src/main/java/com/MBTI/myapplication/list/intj/intj1.java
@@ -2,15 +2,64 @@ package com.MBTI.myapplication.list.intj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.MBTI.myapplication.R;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class intj1 extends AppCompatActivity {
+
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intj1);
+
+        Intent intent = getIntent();
+        ArrayList<String> info = (ArrayList<String>)intent.getSerializableExtra("sVal");
+
+        System.out.println("image string : " + info.get(1));
+        image = findViewById(R.id.image_intj1);
+
+        new DownloadFilesTask().execute(info.get(1));
+    }
+
+    private class DownloadFilesTask extends AsyncTask<String,Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            Bitmap bmp = null;
+            try {
+                String img_url = strings[0]; //url of the image
+                URL url = new URL(img_url);
+                bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return bmp;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            // doInBackground 에서 받아온 total 값 사용 장소
+            image.setImageBitmap(result);
+        }
     }
 }
